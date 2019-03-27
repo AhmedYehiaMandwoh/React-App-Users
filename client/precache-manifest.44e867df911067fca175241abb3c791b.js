3592686bcfdc8bf65965f9f6a88882fc27f91085
Ahmed Yehia <ahmedyahya91@gmail.com>
2019-03-27 19:25:17 +0200

    initial commit

---
+++ client/precache-manifest.44e867df911067fca175241abb3c791b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a6d631700db6eed5f7",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "40a6d631700db6eed5f7",
+    "url": "/static/js/main.6fdbbe50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1acd4566ae5b3f01aca8",
+    "url": "/static/js/2.e11ea581.chunk.js"
+  },
+  {
+    "revision": "70b0b456c2ba527ba80a23e1bd324fb2",
+    "url": "/index.html"
+  }
+];
